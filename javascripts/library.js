2c850fcd7702788b3957ff723a8c07e96685317e
Alex Chaplinsky <alchapone@yandex.ru>
2013-10-25 17:51:44 +0300

    Fixes

--- javascripts/library.js
+++ javascripts/library.js
@@ -1,31 +1,35 @@
 $(document).ready(function(){
+  
+  var Library = {
+    buildSections: function(){
+      $(".library-section").each(function(i, element){
+        Library.setupSection(this, $(element).html(), $(element).data('label'));
+      });
+    },
+    
+    setupSection: function(element, content, label){
+      $(element).html("<div class='library-preview'></div><div class='library-code'></div>");
+      $(element).find('.library-preview').html(content);
+      $(element).find('.library-code').html('<pre><code></code></pre>');
+      $(element).find('.library-code pre code').text(content);
+      $(element).prepend("<span class='library-section-label'>"+label+"</span>");
+    },
 
-  $.when(parseCodeSamples()).done(function(){
-    loadHighlight();
+    loadHighlight: function(){
+      $.getScript("http://yandex.st/highlightjs/7.3/highlight.min.js", function(){
+        hljs.tabReplace = '    ';
+        hljs.initHighlightingOnLoad();
+      });
+    }
+  }
+  
+  $.when(Library.buildSections()).done(function(){
+    Library.loadHighlight();
   });
   
-  function parseCodeSamples() {
-    $(".library-section").each(function(i, element){
-      content = $(element).html();
-      label = $(element).data('label');
-      $(this).html("<div class='library-preview'></div><div class='library-code'></div>");
-      $(this).find('.library-preview').html(content);
-      $(this).find('.library-code').html('<pre><code></code></pre>');
-      $(this).find('.library-code pre code').text(content);
-      $(this).prepend("<span class='library-section-label'>"+label+"</span>");
-    });
-  }
-  
-  function loadHighlight() {
-    $.getScript("http://yandex.st/highlightjs/7.3/highlight.min.js", function(){
-      hljs.tabReplace = '    ';
-      hljs.initHighlightingOnLoad();
-    });
-  }
-
   // Navigation Toggle
-  $('.navigation .expander').click(function(){
-    $('.navigation').toggleClass('expanded');
+  $('.library-navigation .expander').click(function(){
+    $('.library-navigation').toggleClass('expanded');
   });
   
 });
