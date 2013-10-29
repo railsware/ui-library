9e7c5b86686e25f271f42756e5b444f091c13d35
Dmitry Pliska <hornorth@gmail.com>
2013-10-29 14:10:20 +0200

    fix for code sections heights

--- javascripts/library.js
+++ javascripts/library.js
@@ -17,6 +17,7 @@ $(document).ready(function(){
         content = Library.htmlClean($(this).html());
         Library.setupSection(this, content, $(this).data('label'));
         Library.codeSample(this, content);
+        $(this).height($(this).height());
       });
     },
     
